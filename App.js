86d0367fcf6a5dcdb6e48843efe1d6f911fe68f5
BetterSol <94358102+BetterSol@users.noreply.github.com>
2024-03-04 19:06:47 +0200

    switcher from Celsius to Fahrenheit temperature units

--- App.js
+++ App.js
@@ -1,24 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text, TextInput, Button, TouchableOpacity, FlatList, Switch } from 'react-native';
 
 const API_KEY = 'de45b08dad3d48434689160f2237a99e';
 
 export default function WeatherApp() {
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState(null);
-  
-  const fetchWeatherData = async() => {
+  // const [forecast, setForecast] = useState([]);
+  const [enteredCities, setEnteredCities] = useState([]);
+  const [units, setUnits] = useState('metric');
+
+  useEffect(() => {
+    if (city) {
+      fetchWeatherData()
+    }
+  }, [units]);
+
+  const fetchWeatherData = async () => {
     try {
-      const response = await fetch (
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&APPID=${API_KEY}`
       );
       const data = await response.json();
       setWeather(data);
+
+      if (!enteredCities.includes(city)) {
+        setEnteredCities([...enteredCities, city]);
+      }
+
     } catch (error) {
       console.error('error', error);
     }
-  }
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => setCity(item)}>
+      <Text style={styles.item}>{item}</Text>
+    </TouchableOpacity>
+  )
+
+  // const fetchWeatherForecast = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&APPID=${API_KEY}`
+  //     );
+
+  //     const data = await response.json();
+  //     setForecast(data.list);
+  //   } catch (error) {
+  //     console.error('error', error);
+  //   }
+  // }
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -27,25 +60,44 @@ export default function WeatherApp() {
         value={city}
         onChangeText={city => setCity(city)}
       />
-      <Button 
-        title="Get Weather in My City"
-        onPress={fetchWeatherData}
+
+      <Button
+        title="Get Weather in the City"
+        onPress={
+          fetchWeatherData
+        }
       />
+
       {weather && (
-        <View style={styles.weatherContainer}> 
+        <View style={styles.weatherContainer}>
+
+          <View style={styles.switchContainer}>
+            <Text>C</Text>
+            <Switch
+              value={units === 'imperial'}
+              onValueChange={() => setUnits(units === 'metric' ? 'imperial' : 'metric')}
+            />
+            <Text>F</Text>
+          </View>
+          
           <Text>City: {weather.name}</Text>
-          <Text>Temperature: {weather.main.temp}°C</Text>
-          <Text>Weather: {weather.description}</Text>
+          <Text>Temperature: {weather.main.temp}°{units === 'metric' ? 'C' : 'F'}</Text>
+          <Text>Weather: {weather.weather[0].description}</Text>
           <Text>Humidity: {weather.main.humidity}%</Text>
         </View>
       )}
-      <StatusBar style="auto" />
+      <FlatList
+        data={enteredCities}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    top: 200,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
@@ -66,4 +118,15 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     borderColor: '#ccc',
   },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+    gap: 10,
+  },
+  item: {
+    padding: 10,
+    fontSize: 10,
+  }
 });
